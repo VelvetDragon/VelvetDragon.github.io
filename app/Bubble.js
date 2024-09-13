0fc9752d25758abe7b0160423ef4dbd3490a1cdb
Suwan Aryal <aryalsuwan.2003@gmail.com>
2024-09-13 04:50:00 -0500

    Bubble js added

---
+++ app/Bubble.js
@@ -0,0 +1,16 @@
+// Bubbles.js
+import React from 'react';
+
+const Bubbles = () => {
+    return (
+        <div className="bubbles-container">
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+        </div>
+    );
+};
+
+export default Bubbles;
